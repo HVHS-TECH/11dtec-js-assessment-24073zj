cfb174744c023c33b57a9c83a0f97d4bf1f564d8
24073zj <24073zj@hvhs.school.nz>
2026-05-20 00:26:30 +0000

    May 20, 2026, 12:26 AM

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-console.log("Welcome to the shop!")
+const OUTPUT = document.getElementById("devOutput");
+
+var cart = [];
+
+var menuItems = [
+    {
+    name: "Cheeseburger",
+    price: 5
+    },
+    {
+    name: "Double Cheeseburger",
+    price: 7
+    }
+];
 
 const user = {
     name: "",
     moneyEntered: 0
 };
 
-
-
 function AddToCart(_item){
-
+    cart.push(_item);
+}
+function Show()
+{
+    for(let i = 0; i < cart.length(); i++){
+        OUTPUT.innerHTML = i + ":" + cart[i] + "<br>";
+    }
 }
