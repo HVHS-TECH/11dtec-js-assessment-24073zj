06648723182230a0458de6c5c86701e9f3275df7
24073zj <24073zj@hvhs.school.nz>
2026-05-22 02:46:47 +0000

    May 22, 2026, 2:46 AM

--- order.js
+++ order.js
@@ -1,19 +1,42 @@
-var user = sessionStorage.getItem('userData');
+var cart = sessionStorage.getItem('cartData');
+
+const OUTPUT = document.getElementById("devOutput");
+const COST_LABEL = document.getElementById("costLabel");
+
+const menuItems = [
+    { name: "Cheeseburger", price: 5 },
+    { name: "Double Cheeseburger", price: 7 },
+    { name: "Chicken Burger", price: 5 },
+    { name: "Fries", price: 3 },
+    { name: "Coke", price: 5 },
+    { name: "Sprite", price: 5 },
+    { name: "Fanta", price: 5 },
+    { name: "Chocolate Sundae", price: 5 }
+];
+
+var orderPrice = 0;
 
 function CalculateCost(){
     var localPrice = 0;
+
+    /*
     for(let i = 0; i < cart.length; i++){
         let thisItem = menuItems.find(item => item.name === cart[0]);
         localPrice += thisItem.price;
         console.log("a" + localPrice + cart[0] + thisItem.price);
+    }*/
+    for(let i = 0; i < cart.length; i++){
+        if(menuItems.find(item => item.name === cart[i])){
+            let thisItem = menuItems.find(item => item.name === cart[i]);
+            localPrice = thisItem.price;
+        }   
     }
-    user.orderPrice = localPrice;
-    console.log("a" + localPrice);
+    orderPrice = localPrice;
 }
 
 function OrderShow()
 {
-    OUTPUT.innerHTML = "this ran"
+    OUTPUT.innerHTML = ""
     CalculateCost();
-    COST_LABEL.innerHTML = "Your order will cost " + user.orderPrice + " dollars.<br>";
+    COST_LABEL.innerHTML = "Your order will cost " + orderPrice + " dollars.<br>";
 }
--- script.js
+++ script.js
@@ -2,44 +2,8 @@ const MENU_FORM = document.getElementById("menuForm");
 const ORDER_FORM = document.getElementById("orderForm");
 
 const OUTPUT = document.getElementById("devOutput");
-const COST_LABEL = document.getElementById("costLabel");
 
 var cart = [];
-
-const menuItems = [
-    {
-    name: "Cheeseburger",
-    price: 5
-    },
-    {
-    name: "Double Cheeseburger",
-    price: 7
-    },
-    {
-    name: "Chicken Burger",
-    price: 5
-    },
-    {
-    name: "Fries",
-    price: 5
-    },
-    {
-    name: "Coke",
-    price: 5
-    },
-    {
-    name: "Sprite",
-    price: 5
-    },
-    {
-    name: "Fanta",
-    price: 5
-    },
-    {
-    name: "Chocolate Sundae",
-    price: 5
-    }
-];
 
 var user = {
     name: "",
@@ -52,14 +16,15 @@ function AddToCart(_item){
 }
 function MenuShow()
 {
-    OUTPUT.innerHTML = " ";
+    OUTPUT.innerHTML = "Your order:<br>";
     if(cart[0] == undefined){
         OUTPUT.innerHTML = "There is nothing in your cart";
     }
     for(let i = 0; i < cart.length; i++){
         OUTPUT.innerHTML += i + ": " + cart[i] + "<br>";
     }
+    sessionStorage.setItem('cartData', cart);
 }
 
-sessionStorage.setItem('userData', user);
+
 
