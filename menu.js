1952a7786127cd4296aded08dfa205e68f2ea7be
24073zj <24073zj@hvhs.school.nz>
2026-06-15 21:13:56 +0000

    Jun 15, 2026, 9:13 PM

---
+++ menu.js
@@ -0,0 +1,40 @@
+const MENU_FORM = document.getElementById("menuForm");
+const ORDER_FORM = document.getElementById("orderForm");
+
+const OUTPUT = document.getElementById("devOutput");
+
+var cart = [];
+
+var user = {
+    name: "",
+    orderPrice: 0,
+    moneyEntered: 0
+};
+
+function AddToCart(_item){
+    cart.push(_item);
+    MenuShow();
+}
+function MenuShow()
+{
+    OUTPUT.innerHTML = "Your order:<br>";
+    if(cart[0] == undefined){
+        OUTPUT.innerHTML = "There is nothing in your cart";
+        return 1;
+    }
+    for(let i = 0; i < cart.length; i++){
+        OUTPUT.innerHTML += i + ": " + cart[i] + "<br>";
+    }
+    sessionStorage.setItem("cartData", JSON.stringify(cart));
+    return 0;
+}
+
+function NextPage(){
+    let returnValue = MenuShow();
+    if(returnValue != 1){
+        window.location.href="order.html";
+    }
+}
+
+
+
