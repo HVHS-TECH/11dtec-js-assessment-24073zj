18293ed80bdda8c40666812b8aa0117f3e01f180
24073zj <24073zj@hvhs.school.nz>
2026-07-01 00:15:13 +0000

    Jul 1, 2026, 12:15 AM

--- menu.js
+++ menu.js
@@ -46,6 +46,6 @@ function ToggleSection(_num, _this){
     else{
         divArea.style.display = "flex";
         _this.style.borderRadius = "10px 10px 0px 0px";
-        _this.classList.toggle('spin-180');
+        _this.querySelector('img').classList.toggle('spin-180');
     }
 }
